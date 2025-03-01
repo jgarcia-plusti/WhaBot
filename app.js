5bad6fbec843bd4ddef91fbc6d120ae028c14192
Josue Garcia <jgarcia@plusti.com>
2025-02-28 22:23:32 -0600

    IA chat bot

--- app.js
+++ app.js
@@ -1,62 +1,57 @@
-const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot');
+const { EVENTS } = require('@bot-whatsapp/bot');
+const QRPortalWeb = require('@bot-whatsapp/portal');
+const BaileysProvider = require('@bot-whatsapp/provider/baileys');
+const MockAdapter = require('@bot-whatsapp/database/mock');
+const OpenAI = require("openai");
 
-const QRPortalWeb = require('@bot-whatsapp/portal')
-const BaileysProvider = require('@bot-whatsapp/provider/baileys')
-const MockAdapter = require('@bot-whatsapp/database/mock')
+const openai = new OpenAI({
+    baseURL: 'https://api.deepseek.com',
+    apiKey: process.env.OPENAI_API_KEY // Asegúrate de que la API key se obtiene de las variables de entorno
+});
 
-const flowRenovacion = addKeyword(['renovacion', 'renovar', 'actualizar'])
-    .addAnswer([
-        'Si deseas renovar tu plan actual, solo tienes que enviarnos tu boleta de pago con el valor de tu servicio.'
-    ])
-    .addAnswer([
-        'Renovar el mismo',
-        'Agregar un servicio'
-    ])
+const initialPromt = "Eres un asistente virtual para ayudar a mis clientes a saber que plataforma de streaming les conviene, Dales respuestas cortas y consisas " +
+                        "las plataformas son: Netflix Q.35,Disney Q.35,Prime Q.30,HBO Q.25,Spotify Q.40,YouTube Q.25,Crunchyroll Q.25,Paramount Q.25,Deezer Q.25 " + 
+                        "el numero de cuenta es 04103000336503 Banrural y 1229311 Banco Industrial " +
+                        "Convence a los clientes de que compren cualuqiera de las plataformas de streaming "+
+                        "Responde corto y consiso";
 
-const flowPrincipal = addKeyword(['hola', 'ole', 'alo'])
-    .addAnswer([
-        '👋 Hola, te saluda LyJ tu asistente virtual! 🎉',
-        '',
-        '¿En qué puedo asistirte hoy? 🤖',
-    ]).addAnswer([
-        'Renovación',
-        'Servicio Nuevo',
-        'Soporte Tecnico',
-        ''
-    ], null, null, [flowRenovacion, Netflix, Disney])
+msg = [{ role: "user", content: initialPromt }];
+const fn =  async() =>{
+    response = await openai.chat.completions.create({
+        model: 'deepseek-chat',
+        messages: msg
+    });
+}
 
-const Netflix = addKeyword(['netflix', 'net', 'netfli'])
-    .addAnswer([
-        '🎉 ¡Claro! Te puedo ayudar con eso 📱',
-    ]).addAnswer([
-        'Estas son las opciones que tengo para ti:',
-        'Plan Básico','Tiene un costo de Q35 mensuales para un perfil individual.',
-        '',
-        'Promoción especial: Paga 3 meses por tan solo **Q90**.',
-    ])
+fn();
+const REGEX = /.*/;
+const IaFlow = addKeyword(REGEX, { regex: true })
+.addAction({ capture: true }, async (ctx, { flowDynamic }) => {
+    const msgUser = ctx.body;
+    console.log(ctx.body);
+    msg.push({ role: "user", content: msgUser });
+    
+    response = await openai.chat.completions.create({
+        model: 'deepseek-chat',
+        messages: msg
+    });
 
-const Disney = addKeyword(['disney', 'dis', 'disney+'])
-    .addAnswer([
-        '🎉 ¡Claro! Te puedo ayudar con eso 📱'
-    ]).addAnswer([
-        'Tiene un costo de Q30 mensuales para un perfil individual.',
-        '',
-        'Promoción especial: Paga 3 meses por tan solo **Q75**.',
-    ])
-
+    return await flowDynamic(response.choices[0].message.content);
+});
 
 const main = async () => {
-    const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal, Netflix, Disney])
-    const adapterProvider = createProvider(BaileysProvider)
+    const adapterDB = new MockAdapter();
+    const adapterFlow = createFlow([IaFlow]);
+    const adapterProvider = createProvider(BaileysProvider);
 
     createBot({
         flow: adapterFlow,
         provider: adapterProvider,
         database: adapterDB,
-    })
+    });
 
-    QRPortalWeb()
-}
+    QRPortalWeb();
+};
 
-main()
+main();
