5b44ef9e04d2e93bfae991db7114523740e58f5e
Josue Garcia <jgarcia@plusti.com>
2025-02-28 22:39:22 -0600

    IA Chat Bot

--- app.js
+++ app.js
@@ -1,136 +1,48 @@
 const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot');
+const { EVENTS } = require('@bot-whatsapp/bot');
 const QRPortalWeb = require('@bot-whatsapp/portal');
 const BaileysProvider = require('@bot-whatsapp/provider/baileys');
 const MockAdapter = require('@bot-whatsapp/database/mock');
+const OpenAI = require("openai");
 
-const Catalogs = addKeyword(['catalogo', 'catalogos', 'catalogo de servicios', 'catalogo de servicio', 
-    'catalogo de productos', 'catalogo de producto', 'catalogo de planes', 'catalogo de plan', 'Productos', 'productos', 'servicios', 'servicio'])
-    .addAnswer([
-        'Estos son los servicios que tenemos disponibles para ti:',
-        '1. Netflix',
-        '2. Disney+',
-        '3. Prime Video',
-        '4. HBO Max',
-        '5. Spotify',
-        '6. YouTube Premium',
-        '7. Crunchyroll',
-        '8. Paramount+',
-        '9. Deezer',
-        '10. Vix',
-        '',
-        '¿En qué más puedo ayudarte? 🤖',
-    ]);
+const openai = new OpenAI({
+    baseURL: 'https://api.deepseek.com',
+    apiKey: process.env.OPENAI_API_KEY
+});
 
-const Payment = addKeyword(['pago', 'pagar', 'pagos', 'paguar', 'paga', 'pago de servicio', 'pagar servicio', 'pagos de servicio', 'paguar servicio', 'paga servicio'])
-    .addAnswer([
-        'Muchas gracias por confiar en nuestro servicio',
-        '',
-        'Para obtener tu suscripción, por favor realiza el pago de tu servicio.',
-        'Envianos una foto de tu boleta de pago para poder activar el servicio.',
-        'a los numero de cuenta:',
-        '1. Ahorro - Banrural - 04103000336503 - Angel García',
-        '2. Ahorro - B. Industrial - 1229311 - Angel García',
-    ], null, null, [Catalogs]);
+const initialPromt = "Eres un asistente virtual para ayudar a mis clientes a saber que plataforma de streaming les conviene, Dales respuestas cortas y consisas " +
+                        "las plataformas son: Netflix Q.35,Disney Q.35,Prime Q.30,HBO Q.25,Spotify Q.40,YouTube Q.25,Crunchyroll Q.25,Paramount Q.25,Deezer Q.25 " + 
+                        "el numero de cuenta es 04103000336503 Banrural y 1229311 Banco Industrial " +
+                        "Convence a los clientes de que compren cualuqiera de las plataformas de streaming "+
+                        "Responde corto y consiso";
 
-const Accounts = addKeyword(['cuentas', 'cuenta', 'numero de cuenta', 'numero de cuentas', 'cuentas bancarias', 'cuenta bancaria', 'cuentas de banco', 'cuenta de banco'])
-    .addAnswer([
-        'Contamos con cuentas en Banrural y Banco Industrial',
-        'Estamos trabajando para agregar mas bancos a nuestra lista de pagos',
-    ], null, null, [Payment, Catalogs]);
+msg = [{ role: "user", content: initialPromt }];
+const fn =  async() =>{
+    response = await openai.chat.completions.create({
+        model: 'deepseek-chat',
+        messages: msg
+    });
+}
 
-const Bancs = addKeyword(['banco', 'bancos', 'que bancos', 'que banco', 'banco aceptado', 'banco aceptados', 'banco acepta', 'banco aceptan'])
-    .addAnswer([
-        'Por el momento aceptamos pagos en Banrural y Banco Industrial',
-        'pronto agreagaremos mas bancos a nuestra lista de pagos',
-    ], null, null, [Payment, Accounts, Catalogs]);
+fn();
+const REGEX = /.*/;
+const IaFlow = addKeyword([REGEX], { regex: true })
+.addAction({ capture: true }, async (ctx, { flowDynamic }) => {
+    const msgUser = ctx.body;
+    console.log(ctx.body);
+    msg.push({ role: "user", content: msgUser });
+    
+    response = await openai.chat.completions.create({
+        model: 'deepseek-chat',
+        messages: msg
+    });
 
-const Netflix = addKeyword(['netflix', 'netfli', 'nesflix'])
-    .addAnswer([
-        '🎉 ¡Claro! Te puedo ayudar con Netflix 📱',
-    ]).addAnswer([
-        'Estas son las opciones que tengo para ti:',
-        'Plan Básico','Tiene un costo de Q35 mensuales para un perfil individual.',
-        '',
-        'Promoción especial: Paga 3 meses por tan solo **Q90**.',
-    ], null, null, [Payment, Accounts, Bancs, Catalogs]);
-
-const Disney = addKeyword(['disney', 'disney+', 'disneyplus', 'disney plus', 'disnei'])
-    .addAnswer([
-        '🎉 ¡Claro! Te puedo ayudar con Disney 📱'
-    ]).addAnswer([
-        'Tiene un costo de Q30 mensuales para un perfil individual.',
-        '',
-        'Promoción especial: Paga 3 meses por tan solo **Q75**.',
-    ], null, null, [Payment, Accounts, Bancs, Catalogs]);
-
-const ServicesTw = addKeyword(['prime', 'primevideo', 'prime video', 
-    'paramount', 'paramount +', 'paramountplus', 'paramount plus', 
-    'youtube premium', 'youtube premium', 'youtube', 'yutube', 'yotube', 'crunchyroll','crunchy','crunchy roll',
-    'max','hbo','hbomax','hbo max', 'Deezer', 'dezer', 'deser', 'vix', 'vix+', 'vix plus', 'vixplus'])
-    .addAnswer([
-        '🎉 ¡Claro! Te puedo ayudar con eso 📱'
-    ]).addAnswer([
-        'Tiene un costo de Q20 mensuales para un perfil individual.',
-        '',
-        'Promoción especial: Paga 3 meses por tan solo **Q50**.',
-    ], null, null, [Payment, Accounts, Bancs, Catalogs]);
-
-const ServicesTr = addKeyword(['spotify', 'spoti', 'spoty', 'spotifai', 'spotyfai', 'spotyfy', 'spotyfy', 'spotyfay', 'spotyfay',])
-    .addAnswer([
-        '🎉 ¡Claro! Te puedo ayudar con eso 📱'
-    ]).addAnswer([
-        'Tiene un costo de Q40 mensuales para un perfil individual.',
-        '',
-        'Promoción especial: Paga 3 meses por tan solo **Q105**.',
-    ], null, null, [Payment, Accounts, Bancs, Catalogs]);
-
-const NewService = addKeyword(['servicio nuevo', 'nuevo servicio', 'agregar servicio', 'servicio adicional', 'servicio extra', 'nuevo'])
-    .addAnswer([
-        '🎉 ¡Claro! Te puedo ayudar con eso 📱',
-        '',
-        '¿Qué servicio deseas agregar a tu plan actual? 🤖',
-    ], null, null, [Payment, Accounts, Bancs, Catalogs]);
-
-const flowRenovacion = addKeyword(['renovacion', 'renovar'])
-    .addAnswer([
-        'Si deseas renovar tu plan actual, solo tienes que enviarnos tu boleta de pago con el valor de tu servicio.'
-    ])
-    .addAnswer([
-        'Renovar el mismo',
-        'Agregar un servicio'
-    ], null, null, [Payment, Accounts, Bancs, Catalogs]);
-
-const Error = addKeyword(['error', 'errores', 'problema', 'problemas', 'falla', 'fallas', 'no funciona', 'no sirve', 
-    'no puedo', 'no puedo ingresar', 'no puedo acceder', 'no puedo ver'])
-    .addAnswer([
-        '🚨 ¡Lo siento! Parece que tienes un problema con tu servicio 🚨',
-        '',
-        'Por favor, indícame cuál es el problema que tienes para poder ayudarte. 🤖',
-    ]);
-
-const Adquirir = addKeyword(['adquirir', 'comprar', 'compra', 'adquirir servicio', 'comprar servicio', 'compra servicio', 
-    'adquirir un servicio', 'comprar un servicio', 'compra un servicio', 'quiero adquirir', 'quiero comprar', 'quiero compra'])
-    .addAnswer([
-        '¡Claro! Te puedo ayudar 📱',
-        '',
-        '¿Qué servicio deseas adquirir? 🤖',
-    ], null, null, [Payment, Accounts, Bancs, Catalogs]);
-
-const flowPrincipal = addKeyword(['hola', 'ola', 'buenas tardes', 'buenos dias', 'buenas noches', 'buenas', 'hi', 'hello'])
-    .addAnswer([
-        '👋 Hola, te saluda LyJ tu asistente virtual! 🎉',
-        '',
-        '¿En qué puedo asistirte hoy? 🤖',
-    ]).addAnswer([
-        'Renovación',
-        'Servicio Nuevo',
-        'Soporte Tecnico',
-        ''
-    ], null, null, [Catalogs, Adquirir, Error, NewService, flowRenovacion, Netflix, Disney, ServicesTw, ServicesTr, Payment, Accounts, Bancs]);
+    return await flowDynamic(response.choices[0].message.content);
+});
 
 const main = async () => {
     const adapterDB = new MockAdapter();
-    const adapterFlow = createFlow([flowPrincipal, Payment, Accounts, Bancs]);
+    const adapterFlow = createFlow([IaFlow]);
     const adapterProvider = createProvider(BaileysProvider);
 
     createBot({
